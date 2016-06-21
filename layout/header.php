da3edcc2267a8bb0606bbda2747f538129bc1569
Taras Holets <taras.holets@gmail.com>
2016-06-21 21:19:01 +0300

    first commit

---
+++ layout/header.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		
+		<title>Dnipro LLC</title>
+
+		<link href="css/normalize.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/iphone4.css" media="only screen and (max-width:320px)">
+        <link rel="stylesheet" href="css/iphone480.css" media="only screen and (max-width:480px)">
+		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.5/assets/owl.carousel.min.css">
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+        
+	</head>
+
+	   <body>
+            <main>
+                <header class="root">
+                    <div class="container" >
+                        <div class="clearfix">
+                            <div>
+                                <img class="logo" src="images/logo.png" alt="">
+                            </div>
+                            <div>
+                                <img class="logo1" src="images/logo-footer.png" alt="">
+                            </div>
+                            <div id="time">
+                                <div id="center"></div>
+                                <div id="second"></div>
+                                <div id="minute"></div>
+                                <div id="hour"></div>
+                            </div>
+                            <div class="top">
+                                <nav class="primary">
+                                    <ul>
+                                        <li><a href="#"><img class="image-top" src="images/Kabinet.png">Кабінет</a></li>
+                                        <li><a href><img class="image-top" src="images/map.png">Трекінг</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/calculator.png">Вартість</a></li>
+                                        <li><a href="#"><img class="image-top" src="images/help.png">Підтримка</a></li>
+                                        <li class="menu-btn">
+                                            <i class="fa fa-bars" aria-hidden="true"> Меню</i>
+                                        </li>
+                                </ul>              
+                                </nav>
+                            </div>                
+                            <div class="botton">
+                                <nav class="botton-nav">
+                                    <ul>
+                                        <li><a href="#">Сервіси <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Авіа Перевезення</a></li>
+                                        <li><a href="#">Морські Перевезення <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Контейнерні</a></li>
+                                                <li><a href="#">Ro-Ro/Lo-Lo</a></li>
+                                            </ul>
+                                        </li>  
+                                                <li><a href="#">Перевезення по США</a></li>
+                                        <li><a href="#">Доставка посилок з США <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul class="but-meny">
+                                                <li><a href="#">Доставка в Україну</a></li>
+                                                <li><a href="#">Доставка в Росію</a></li>
+                                                <li><a href="#">Доставка в Білорусь</a></li>
+                                                <li><a href="#">Доставка в Молдову</a></li>
+                                                <li><a href="#">Доставка в Грузію</a></li>
+                                                <li><a href="#">Доставка в країни Євросоюзу</a></li>
+                                                <li><a href="#">Доставка в країни Середньої Азії</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Складські послуги <i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                                            <ul class="poslugi">
+                                                <li><a href="#">Адреса в США</a></li>
+                                                <li><a href="#">Зберігання</a></li>
+                                                <li><a href="#">Консолідація</a></li>
+                                                <li><a href="#">Перепакування</a></li>
+                                            </ul>
+                                                </li>
+                                            </ul>
+                                        </li>                            
+                                        <li><a href="#"><i class="fa fa-shopping-cart" style="color: #a6ce39" aria-hidden="true"></i>  Amazon&amp;eBay</a></li>        
+                                        <li><a href="#"><i class="fa fa-car" style="color: #a6ce39" aria-hidden="true"></i>  Авто-мото <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul>
+                                                <li><a href="#">Доставка Автомобілів</a></li>
+                                                <li><a href="#">Доставка Мотоциклів </a></li>
+                                                <li><a href="#">Доставка Лодок </a></li>
+                                                <li><a href="#">Перевезення авто по США </a></li>
+                                                <li><a href="#">Доставка Спец.Техніки </a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Блог</a></li>
+                                        <li><a href="#">Контакти</a></li>
+                                        <li class="search">
+                                        <i class="fa fa-search" style="color: #a6ce39" aria-hidden="true"></i>
+                                        <input type="search" id="search" placeholder="Пошук..." />
+                                        </li>
+                                        <li><a href="#" style="color: #0072bc">укр</a></li>
+                                        <li><a href="#" style="color: #0072bc">рус</a></li>
+                                        <li><a href="#" style="color: #0072bc">eng</a></li>
+
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </main>
+
+<nav class="pushy pushy-right">
+            <ul>
+                <li class="pushy-link"><a href="#">Авіа Перевезення</a></li>
+                <li class="pushy-submenu">
+                    <a href="#">Морські Перевезення</a>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Контейнерні</a></li>
+                        <li class="pushy-link"><a href="#">Ro-Ro/Lo-Lo</a></li>
+                    </ul>
+                </li>
+                 <li class="pushy-link"><a href="#">Перевезення по США</a></li>
+                <li class="pushy-submenu">
+                    <a href="#">Доставка посилок з США</a>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Доставка в Україну</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в Росію</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в Білорусь</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в Молдову</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в Грузію</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в країни Євросоюзу</a></li>
+                        <li class="pushy-link"><a href="#">Доставка в країни Середньої Азії</a></li>
+                    </ul>
+                </li>
+                <li class="pushy-submenu">
+                    <a href="#">Складські послуги</a>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Адреса в США</a></li>
+                        <li class="pushy-link"><a href="#">Зберігання</a></li>
+                        <li class="pushy-link"><a href="#">Консолідація</a></li>
+                        <li class="pushy-link"><a href="#">Перепакування</a></li>
+                    </ul>
+                </li>
+           
+            </li>    
+                <li class="pushy-link"><a href="#"><i class="fa fa-shopping-cart" style="color: #a6ce39" aria-hidden="true"></i>  Amazon&amp;eBay</a></li>
+                <li class="pushy-submenu"><a href="#"><i class="fa fa-car" style="color: #a6ce39" aria-hidden="true"></i> Авто-мото</a>
+                    <ul>
+                        <li class="pushy-link"><a href="#">Доставка Автомобілів</a></li>
+                        <li class="pushy-link"><a href="#">Доставка Мотоциклів</a></li>
+                        <li class="pushy-link"><a href="#">Доставка Лодок </a></li>
+                        <li class="pushy-link"><a href="#">Перевезення авто по США</a></li>
+                        <li class="pushy-link"><a href="#">Доставка Спец.Техніки</a></li>
+                    </ul>
+                </li>
+                <li class="pushy-link"><a href="#">Блог</a></li>
+                <li class="pushy-link"><a href="#">Контакти</a></li>
+            </ul>
+
+        </nav>
+
+
+        <!-- Site Overlay -->
+        <div class="site-overlay"></div>
+
+
+
+    <!-- Pushy JS -->
+        <script src="js/pushy.min.js"></script>
+
+
+
+<script>
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 20){
+            $('header').addClass("sticky");
+     }
+        else{
+            $('header').removeClass("sticky");
+     }
+    });
+</script>
+
+
+
+
+
+
+
+<script type="text/javascript">
+    $(document).ready(function($) {
+setInterval( function() {
+      var seconds = new Date().getSeconds();
+      var sdegree = seconds * 6;
+      var srotate = 'rotate(' + sdegree + 'deg)';
+
+      $('#second').css({'-moz-transform' : srotate, '-webkit-transform' : srotate, '-o-transform' : srotate});
+
+      }, 1000 );
+
+      setInterval( function() {
+      var hours = new Date().getHours();
+      var mins = new Date().getMinutes();
+      var hdegree = hours * 30 + (mins / 2);
+      var hrotate = 'rotate(' + hdegree + 'deg)';
+
+      $('#hour').css({'-moz-transform' : hrotate, '-webkit-transform' : hrotate, '-o-transform' : hrotate});
+
+      }, 1000 );
+
+      setInterval( function() {
+      var mins = new Date().getMinutes();
+      var mdegree = mins * 6;
+      var mrotate = 'rotate(' + mdegree + 'deg)';
+
+      $('#minute').css({'-moz-transform' : mrotate, '-webkit-transform' : mrotate, '-o-transform' : mrotate});
+
+      }, 1000 );
+    
+    for (i=1;i<=6;i++) {
+        $('#center').before('<div id="l'+i+'"></div>');
+    }
+    for (i=1;i<=30;i++) {
+        $('#center').before('<div id="ln'+i+'"></div>');
+        $('#ln'+i).css({
+            "-moz-transform" : "rotate(" + i*6 + "deg)", 
+            "-webkit-transform" : "rotate(" + i*6 + "deg)", 
+            "-o-transform" : "rotate(" + i*6 + "deg)", 
+            "padding" : "60px 1px 60px 0", 
+            "left" : "60px", 
+            "position" : "absolute",
+            "display" : "block",
+            "content" : "",
+       });
+    }
+});
+
+
+</script>
